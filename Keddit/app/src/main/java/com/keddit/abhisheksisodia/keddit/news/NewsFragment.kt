ecea67eea9d0fa21f239075eaf70fe6195392d14
Abhishek Sisodia <abhishek.sisodia@scotiabank.com>
2018-07-02 19:01:50 -0400

    added lazy delegate for newsList and using android extension to access news_list

--- Keddit/app/src/main/java/com/keddit/abhisheksisodia/keddit/news/NewsFragment.kt
+++ Keddit/app/src/main/java/com/keddit/abhisheksisodia/keddit/news/NewsFragment.kt
@@ -3,24 +3,28 @@ package com.keddit.abhisheksisodia.keddit.news
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.keddit.abhisheksisodia.keddit.R
 import com.keddit.abhisheksisodia.keddit.common.inflate
+import kotlinx.android.synthetic.main.news_fragment.*
 
 class NewsFragment : Fragment() {
 
-    private var newsList: RecyclerView? = null
+    private val newsList by lazy {
+        news_list
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         //val view = inflater.inflate(R.layout.news_fragment, container, false)
-        val view = container?.inflate(R.layout.news_fragment)
-        newsList = view?.findViewById(R.id.news_list) as RecyclerView?
-        newsList?.setHasFixedSize(true) // use this setting to improve performance
-        newsList?.layoutManager = LinearLayoutManager(context)
+        return container?.inflate(R.layout.news_fragment)
+    }
 
-        return view
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        newsList.setHasFixedSize(true)
+        newsList.layoutManager = LinearLayoutManager(context)
     }
 }
