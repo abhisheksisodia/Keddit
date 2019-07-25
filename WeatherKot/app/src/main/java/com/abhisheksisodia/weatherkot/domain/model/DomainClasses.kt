e809b8e7e4dc7fa01aae019d26d527b6f7c83772
Abhishek Sisodia <abhishek.sisodia@scotiabank.com>
2019-07-24 20:47:40 -0400

    Added forecast list and updated forecast list adapter

--- WeatherKot/app/src/main/java/com/abhisheksisodia/weatherkot/domain/model/DomainClasses.kt
+++ WeatherKot/app/src/main/java/com/abhisheksisodia/weatherkot/domain/model/DomainClasses.kt
@@ -1,4 +1,19 @@
 package com.abhisheksisodia.weatherkot.domain.model
 
-data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>)
-data class Forecast(val date: String, val description: String, val high: Int, val low: Int)
+data class ForecastList(
+    val city: String, val country: String,
+    private val dailyForecast: List<Forecast>
+) {
+
+    //property for easy access
+    val size: Int
+        get() = dailyForecast.size
+
+    //operator overloading
+    operator fun get(position: Int): Forecast = dailyForecast[position]
+}
+
+data class Forecast(
+    val date: String, val description: String, val high: Int, val low: Int,
+    val iconUrl: String
+)
--- WeatherKot/app/src/main/java/com/abhisheksisodia/weatherkot/ui/activities/MainActivity.kt
+++ WeatherKot/app/src/main/java/com/abhisheksisodia/weatherkot/ui/activities/MainActivity.kt
@@ -6,9 +6,11 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.abhisheksisodia.weatherkot.R
 import com.abhisheksisodia.weatherkot.domain.commands.RequestForecastCommand
+import com.abhisheksisodia.weatherkot.domain.model.Forecast
 import com.abhisheksisodia.weatherkot.ui.adapters.ForecastListAdapter
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.find
+import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
@@ -23,7 +25,12 @@ class MainActivity : AppCompatActivity() {
         doAsync {
             val result = RequestForecastCommand("94043").execute()
             uiThread {
-                forecastList.adapter = ForecastListAdapter(result)
+                forecastList.adapter = ForecastListAdapter(result,
+                    object : ForecastListAdapter.OnItemClickListener {
+                        override fun invoke(forecast: Forecast) {
+                            toast(forecast.date)
+                        }
+                    })
             }
         }
     }
